89af73770b08d741838994edd5351148738501de
Azizkhaz <khalfaouia362@gmail.com>
2024-05-16 13:30:53 +0200

    first commit

--- node.js
+++ node.js
@@ -45,3 +45,36 @@ function generateRandomPassword() {
 // Call the function to generate and log a random password
 generateRandomPassword();
 
+const nodemailer = require('nodemailer');
+
+// Create a transporter object using the SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587, // or 465 for secure
+    secure: false, // true for port 465, false for other ports
+    auth: {
+        user: 'khalfaouiazoz22@gmail.com', // your email address
+        pass: '' // your email password or app password
+    },
+    tls: {
+        rejectUnauthorized: false // allow self-signed certificates
+    }
+});
+
+// Set up email data with unicode symbols
+let mailOptions = {
+    from: '"" <>', // sender address
+    to: '', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>' // html body
+};
+
+// Send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+});
